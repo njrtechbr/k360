da6561b8b27a73a876a9a9928ed8ec04c9f9a330
Nereu Jr <nnvljr86@gmail.com>
2025-08-28 20:10:24 +0000

    Error: React.Children.only expected to receive a single React element ch

--- src/app/dashboard/modulos/page.tsx
+++ src/app/dashboard/modulos/page.tsx
@@ -206,24 +206,32 @@ export default function ModulosPage() {
                 </DialogHeader>
                  <Form {...editForm}>
                     <form onSubmit={editForm.handleSubmit(onEditSubmit)} className="space-y-4 pt-4">
-                        <FormField control={editForm.control} name="name" render={({ field }) => ( 
-                            <FormItem> 
-                                <FormLabel>Nome do Módulo</FormLabel> 
-                                <FormControl> 
-                                    <Input {...field} /> 
-                                </FormControl> 
-                                <FormMessage /> 
-                            </FormItem> 
-                        )}/>
-                        <FormField control={editForm.control} name="description" render={({ field }) => ( 
-                            <FormItem> 
-                                <FormLabel>Descrição</FormLabel> 
-                                <FormControl> 
-                                    <Textarea {...field} /> 
-                                </FormControl> 
-                                <FormMessage /> 
-                            </FormItem> 
-                        )}/>
+                       <FormField
+                          control={editForm.control}
+                          name="name"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Nome do Módulo</FormLabel>
+                              <FormControl>
+                                <Input {...field} />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <FormField
+                          control={editForm.control}
+                          name="description"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Descrição</FormLabel>
+                              <FormControl>
+                                <Textarea {...field} />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
                         <DialogFooter>
                             <Button type="button" variant="secondary" onClick={() => setIsEditDialogOpen(false)}>Cancelar</Button>
                             <Button type="submit" disabled={editForm.formState.isSubmitting}>
